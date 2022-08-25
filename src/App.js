51de805de341421a5293e1eae6903ba38be16e47
ClydeError <peterlishman18@gmail.com>
2022-08-25 13:41:30 +0300

    Firs commit v 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./components/Layout.jsx";
+import {Routes, Route} from 'react-router-dom';
+
+
+
+import MainPage from './pages/MainPage'
+import PostPage from "./pages/PostPage";
+import PostsPage from './pages/PostsPage'
+import AddPostPage from "./pages/AddPostPage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import EditPostPage from "./pages/EditPostPage";
+import { ToastContainer } from 'react-toastify';
+
+//TODO вернуть Layout
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <Layout>
+            <Routes>
+                {/*Убрать (/) со всех роутов*/}
+                <Route path='/' element={<MainPage/>}/>
+                <Route path='/posts' element={<PostsPage/>}/>
+                <Route path='/:id' element={<PostPage/>}/>
+                <Route path='/:id/edit' element={<EditPostPage/>}/>
+                <Route path='/new' element={<AddPostPage/>}/>
+                <Route path='/register' element={<RegisterPage/>}/>
+                <Route path='/login' element={<LoginPage/>}/>
+            </Routes>
+            <ToastContainer position='bottom-right' />
+        </Layout>
+    )
+
+
 }
+
 
 export default App;
