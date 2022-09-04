b0517cc795e2c9f9ebb75e6cfd10ddeeca087169
ClydeError <peterlishman18@gmail.com>
2022-09-05 02:08:52 +0300

    get posts to Front

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const MainPage = () => {
     return (
-        <div>
-            MainPage
+        <div className='max-w-[900px] mx-auto py-10'>
+            <div className="flex justify-between gap-8">
+                <div className="flex flex-col gap-10 basis-4/5">
+                    Posts
+                </div>
+                <div className="basis-1/5">
+                    <div className='text-xs uppercase text-white'>
+                        Популярное:
+                    </div>
+                    Popular Posts
+                </div>
+            </div>
         </div>
     );
 };
--- src/redux/features/post/postSlice.js
+++ src/redux/features/post/postSlice.js
@@ -1,4 +1,4 @@
-import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import axios from "../../../utils/axios";
 
 const initialState = {
@@ -10,11 +10,22 @@ const initialState = {
 
 export const createPost = createAsyncThunk(
     'post/createPost',
-    async(params) => {
-    try {
-        const { data } = await axios.post('/posts', params)
+    async (params) => {
+        try {
+            const {data} = await axios.post('/posts', params)
 
+            return data
+        } catch (error) {
+            console.log(error)
+        }
+    })
+
+export const getAllPosts = createAsyncThunk('post/getAllPosts', async () => {
+    try {
+
+        const {data} = await axios.get('/posts')
         return data
+
 
     } catch (error) {
         console.log(error)
@@ -22,11 +33,44 @@ export const createPost = createAsyncThunk(
 })
 
 
+
 export const postSlice = createSlice({
-    name: 'post',
-    initialState,
-    reducers: {},
-    extraReducers: {},
-})
+        name: 'post',
+        initialState,
+        reducers: {},
+        extraReducers: {
+
+            //Create Post
+
+            [createPost.pending]: (state) => {
+                state.loading = true
+            },
+            [createPost.fulfilled]: (state, action) => {
+                state.loading = false
+                state.post.push(action.payload)
+            },
+            [createPost.rejected]: (state) => {
+                state.loading = false
+
+            },
+
+            //Get all Posts
+
+            [getAllPosts.pending]: (state) => {
+                state.loading = true
+            },
+            [getAllPosts.fulfilled]: (state, action) => {
+                state.loading = false
+                state.post = action.payload.posts
+                state.popularPosts = action.payload.popularPosts
+            },
+            [getAllPosts.rejected]: (state) => {
+                state.loading = false
+
+            },
+
+        }
+    }
+)
 
 export default postSlice.reducer
